14332646ddfcf3fda982c36b12257be8596e2202
zion0624 <34365217+zion0624@users.noreply.github.com>
2017-12-09 22:24:47 +0900

    container
    I can make a list

---
+++ container.py
@@ -0,0 +1,10 @@
+print(type('zion'))
+name='zion'
+print(name)
+print(type(['zion','jspark','jungsam']))
+names=['zion','jspark','jungsam']
+print(names)
+print(names[1])
+zion=['student','ansan',20,True] #하나의 리스트안에 문자열 숫자 블리언 등을 같이 쓸 수있다.
+zion[1]='busan'
+print(zion)
