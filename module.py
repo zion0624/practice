e425f8d16e22e34dd8c4c06a497cea96f66ec9c9
zion0624 <34365217+zion0624@users.noreply.github.com>
2017-12-20 00:06:08 +0900

    module
    모듈에 관한 수업을 들었다
    
    요즘이런이유 저런이유 때문에 프로그래밍 연습을 오래동안 못했다 시작한지 며칠 되지않았는데 바로 이렣게 긴 시간동안 공부를 못했다는게 안타깝고 반성된다 앞으로 열심히하자

---
+++ module.py
@@ -0,0 +1,8 @@
+import math
+print(math.ceil(2.1))
+print(math.floor(2.1))
+print(math.sqrt(4))   #제곱근
+
+import function #import 뒤에 가져올 파일등을 쓰면 그 파일을 가져온다
+from function import a3  # function파일의 일부만을 가져오고 싶으면 from을 사용한다
+print(a3())         # !!!일부만을 가져오고 싶었는데 실패...
